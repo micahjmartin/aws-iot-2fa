961746eb5ee3b9d1323c943b8a19471e21f954e1
MCosmadelis <mjc4949@g.rit.edu>
2018-09-19 13:01:34 -0400

    basic login functionality

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('user')
+        password = request.form.get('password')
+        if user == "dad" and password == "password":
+            return "User logged in, issue a cookie"
+        return "User not authorized (will redirect back to login page)"
+    else:
+        return "Login page goes here"
+
